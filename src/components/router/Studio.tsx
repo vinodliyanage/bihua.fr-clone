e391174af7e03ea5fa5c6791da60b7eb13e03404
Vinod Liyanage <vinodsliyanage@gmail.com>
2022-08-06 18:27:23 +0530

    update the Studio Component.

--- src/components/router/Studio.tsx
+++ src/components/router/Studio.tsx
@@ -1,15 +1,36 @@
 import React from "react";
-import Hero from "../layout/HeroTitle";
+import Base from "../layout/Base";
+import Final from "../layout/Final";
+import Passion from "../layout/Passion";
+import Team from "../layout/Team";
 
 const Studio: React.FC = () => {
   return (
-    <div className="studio block">
-      <Hero title="Bihua Transforme vos idées - en image" className="studio">
-        <span className="word">Apprendre les règles </span>
-        <span className="word shift-right"> comme un professionnel </span>
-        <span className="word"> et les enfreindre comme </span>
-        <span className="word shift-right"> — un artiste </span>
-      </Hero>
+    <div className="studio">
+      <section className="studio__hero block">
+        <h1 className="title">
+          <span className="word">Apprendre les règles </span>
+          <span className="word shift-right">comme un</span>
+          <span className="word">professionnel</span>
+          <span> </span>
+          <span className="word">et les </span>
+          <span> </span>
+          <span className="word"> enfreindre comme</span>
+          <span className="word shift-right"> — un artiste </span>
+        </h1>
+      </section>
+      <section className="studio__base block">
+        <Base />
+      </section>
+      <section className="studio__team">
+        <Team />
+      </section>
+      <section className="studio__passion">
+        <Passion />
+      </section>
+      <section className="studio__final block">
+        <Final />
+      </section>
     </div>
   );
 };
