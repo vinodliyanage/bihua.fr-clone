faabc3d7e2ed5a4534fb03ceedc79b5643a25cb1
Vinod Liyanage <vinodsliyanage@gmail.com>
2022-08-08 11:41:52 +0530

    create Error Component.

--- src/components/layout/Error.tsx
+++ src/components/layout/Error.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Error: React.FC = () => {
-  return <h1>error 404</h1>;
+  const { pathname } = useLocation();
+  return (
+    <section className="error block">
+      <div className="error__wrapper">
+        <div className="error__content__container">
+          <h1 className="error-title">
+            Désolé,
+            <br />
+            une erreur est survenue:
+          </h1>
+          <p className="error-content">
+            La page à l'adresse
+            <span> &quot;</span>
+            {pathname}
+            <span>&quot; </span>
+            n'as pas était trouvée !
+            <br />
+            Nous vous invitons à retourner sur la page d' Accueil ou la page de
+            nos Réalisations !
+          </p>
+        </div>
+        <div>
+          <p className="error-code">404</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Error;
