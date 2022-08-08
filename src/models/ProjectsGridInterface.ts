484c087051be460a7a7b8ff16536beebce0b77bb
Vinod Liyanage <vinodsliyanage@gmail.com>
2022-08-08 14:04:07 +0530

    refactor the interface and rename filename.

--- src/models/ProjectsGridInterface.ts
+++ src/models/ProjectsGridInterface.ts
@@ -1,12 +1,8 @@
 import { GridInterface } from "./GridInterface";
 import { ProjectInterface } from "./ProjectInterface";
 
-interface ProjectsGrid {
+export default interface ProjectsGrid {
   projects: ProjectInterface[];
   grid: GridInterface[];
 }
 
-export interface ProjectsInterface {
-  projectsGrid:ProjectsGrid,
-  rowSize: number;
-}
